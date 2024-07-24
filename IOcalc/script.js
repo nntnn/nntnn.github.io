37f37f3866ad2755d38655a649b8fa1b4e508ea8
nntnn <garamdang@gmail.com>
2024-07-24 22:24:52 +0900

    Update script.js

--- IOcalc/script.js
+++ IOcalc/script.js
@@ -1,12 +1,15 @@
 function calculateProfit() {
     const initialValue = parseFloat(document.getElementById('initial-value').value);
+    const cumValue = parseFloat(document.getElementById('cumulative').value);
+    const years = parseFloat(document.getElementById('years').value);
     const returns = parseFloat(document.getElementById('returns').value);
+    const retp = returns + 1
     
     if (isNaN(initialValue) || isNaN(returns)) {
         document.getElementById('result').innerText = 'Please enter valid numbers.';
         return;
     }
 
-    const cumulativeProfit = initialValue * (1 + returns / 100);
+    const cumulativeProfit = initialValue * (retp) ^ years + (cumValue)*(1-retp^years)/(1-retp);
     document.getElementById('result').innerText = `Cumulative Profit: $${cumulativeProfit.toFixed(2)}`;
 }
